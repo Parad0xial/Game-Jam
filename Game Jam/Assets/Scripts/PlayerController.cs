7081ea5f51cb81fe5afefa195629eac9dad02032
Parad0xial <64382080+Parad0xial@users.noreply.github.com>
2020-07-10 13:07:27 -0700

    controls are finished pogchamp

--- Game Jam/Assets/Scripts/PlayerController.cs
+++ Game Jam/Assets/Scripts/PlayerController.cs
@@ -17,8 +17,26 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate(){
      moveInput = Input.GetAxisRaw("Horizontal");
-     rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+
+    if (Input.GetKey(KeyCode.D))
+         {
+             rb.velocity = new Vector2(speed, rb.velocity.y);
+         }
+ 
+    if (Input.GetKey(KeyCode.A))
+         {
+             rb.velocity = new Vector2(-speed, rb.velocity.y);
+         }
+    
+    if (Input.GetKeyDown("space"))
+         {
+             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+         }
      
-     speed = speed + speedIncrease;
+     if(moveInput != 0){
+      speed = speed + speedIncrease;
+	 }
+     
 	}
 }
