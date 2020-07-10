9eeaa02d23b54817425d599efae6cbdcc5ecee62
Parad0xial <64382080+Parad0xial@users.noreply.github.com>
2020-07-10 12:44:59 -0700

    added player controller

---
+++ Game Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speedIncrease;
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    public Rigidbody2D rb;
+
+    void start(){
+     rb = GetComponent<Rigidbody2D>(); 
+	}
+
+    void FixedUpdate(){
+     moveInput = Input.GetAxisRaw("Horizontal");
+     rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+     
+     speed = speed + speedIncrease;
+	}
+}
