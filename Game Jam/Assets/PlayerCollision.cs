8d6bf6adff8d6d0060cc6531be8aba4e95021641
MageyMage <64452472+MageyMage@users.noreply.github.com>
2020-07-10 14:33:48 -0700

    CollisionScript
    Wrote a collision script for the player based around tags.

---
+++ Game Jam/Assets/PlayerCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.collider.tag == "Obstacle"){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
