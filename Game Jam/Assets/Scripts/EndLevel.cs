5c97817808bb35d5a30724c7bad9b2e585606750
MageyMage <64452472+MageyMage@users.noreply.github.com>
2020-07-11 15:19:00 -0700

    Level 2

---
+++ Game Jam/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    void LoadNextLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
