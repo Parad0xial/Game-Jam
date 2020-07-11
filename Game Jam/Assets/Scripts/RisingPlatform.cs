830855a05810faa3bdfaa2d83401131c3cdc194f
MageyMage <64452472+MageyMage@users.noreply.github.com>
2020-07-11 13:28:41 -0700

    Level 2

---
+++ Game Jam/Assets/Scripts/RisingPlatform.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class RisingPlatform : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float risingspeed;
+
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.collider.tag == "Player"){
+          rb.velocity = new Vector2(risingspeed, rb.velocity.x);
+          }
+        }
+
+}
