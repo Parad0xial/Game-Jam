6f047f71b75883ef3307ff540a2aae2d759f0fa3
Parad0xial <64382080+Parad0xial@users.noreply.github.com>
2020-07-11 08:09:16 -0700

    tried a camera script

---
+++ Game Jam/Assets/Scripts/CameraSize.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSize : MonoBehaviour
+{
+    public float offsetAdd;
+    public float offsetMultiply;
+    public Camera m_OrthographicCamera;
+
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        m_OrthographicCamera.orthographicSize = 15.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_OrthographicCamera.orthographicSize = rb.velocity.x + 5f;
+    }
+}
