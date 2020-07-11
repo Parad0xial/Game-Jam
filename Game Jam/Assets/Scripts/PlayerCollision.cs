58729201fab173c718d27960b50c4c9d92bed0bd
MageyMage <64452472+MageyMage@users.noreply.github.com>
2020-07-11 12:01:02 -0700

    Animations

--- Game Jam/Assets/Scripts/PlayerCollision.cs
+++ Game Jam/Assets/Scripts/PlayerCollision.cs
@@ -4,8 +4,20 @@ using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D col){
+
+    public Animator animator;
+    private Collision2D col;
+    public PlayerController movement;
+
+    void Start()
+    {
+        StartCoroutine(OnCollisionEnter2D(col));
+    }
+
+    public IEnumerator OnCollisionEnter2D(Collision2D col){
         if(col.collider.tag == "Obstacle"){
+            animator.SetBool("IsDead", true);
+            yield return new WaitForSeconds(.44f);
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
